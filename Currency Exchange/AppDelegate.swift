1ba7e9be2423e5fc69f663ea0b5c1cf328bf15e2
Ivan <ivan.kyryliv@pettersonapps.com>
2022-10-10 14:05:04 +0300

    initial commit

---
+++ Currency Exchange/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Currency Exchange
+//
+//  Created by Ivan on 10.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
