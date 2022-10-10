18f98c4f351365fea83aca4dfe71e24e7cbd7565
Ivan <ivan.kyryliv@pettersonapps.com>
2022-10-10 15:37:16 +0300

    added scheme

--- Currency Exchange/AppDelegate.swift
+++ Currency Exchange/AppDelegate.swift
@@ -9,11 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+#if DEV
+        print("DEV Scheme")
+#elseif PROD
+        print("PROD Scheme")
+#endif
         return true
     }
 }
